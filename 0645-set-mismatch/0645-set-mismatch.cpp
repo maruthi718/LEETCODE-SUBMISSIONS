681026689923ac11f004602f04a0f37f5b63d1ba
Maruthi Kunuku <113303489+maruthi718@users.noreply.github.com>
2022-10-24 12:37:47 +0530

    Time: 171 ms (13.40%), Space: 30.9 MB (21.42%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,49 +1,19 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         vector<int>ans;
-       long n= nums.size();
-       for(int i=0;i<n-1;i++)
-       {
-           if(nums[i]==nums[i+1]) 
-           {
-               ans.emplace_back(nums[i]); 
-                    if(nums[n-1]==n-1)
-                       ans.emplace_back(n); 
-                   else if(i+1<nums[i])
-                   {
-                       if(nums[0]!=1)
-                       {
-                          ans.emplace_back(1); 
-                          break;
-                       }
-                        for(int j=0;j<i;j++)
-                       {
-                          if(nums[j]+1!=nums[j+1])
-                          {
-                            ans.emplace_back(nums[j]+1); 
-                             break; 
-                          }   
-                       }
-                   }        
-               else
-               {
-                   for(int j=i+1;j<n-1;j++)
-                   {
-                       
-                       if(nums[j]+1!=nums[j+1])
-                       {
-                            ans.emplace_back(nums[j]+1); 
-                       break; 
-                       }
-                   }
-               }
-               break;
-           }
-       }
-       
-        return ans; 
-    
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        int actsum=(n*(n+1)/2);
+        int sum=0,x;
+        for(int i=0;i<n;i++) 
+        {
+            sum+=nums[i];
+           mp[nums[i]]++;
+            if(mp[nums[i]]==2)
+            x=nums[i];
+                
+        }
+        return {x,actsum-sum+x}; 
     }
 };
