235bb26382801ffcb6edf81a8bff3143bea93d80
Maruthi Kunuku <113303489+maruthi718@users.noreply.github.com>
2022-12-27 00:00:28 +0530

    Time: 8 ms (86.24%), Space: 7.4 MB (44.07%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    bool checkInclusion(string s1, string s2) {
-        int m=s1.size();
-        int n=s2.size();
-        if(m>n)
-            return false;
-        else if(m==n)
+    bool checkInclusion(string p, string s) {
+        vector<int>ans;
+        long n=s.size();
+        long m=p.size();
+        if(n<m)
+         return false;
+        vector<int>mp1(26,0);
+        vector<int>mp2(26,0);
+        for(int i=0;i<m;i++)
         {
-            sort(s1.begin(),s1.end());
-            sort(s2.begin(),s2.end());
-            if(s1==s2)
-                return true;
-            return false;
+            mp1[s[i]-'a']++;
+            mp2[p[i]-'a']++; 
         }
-        sort(s1.begin(),s1.end());
-        for(int i=0;i<n-m+1;i++)
+        if(mp1==mp2) return true;
+        for(int i=m;i<n;i++)
         {
-            string temp=s2.substr(i,m);
-            sort(temp.begin(),temp.end());
-            if(temp==s1)
-                return true;
+           mp1[s[i]-'a']++;
+             mp1[s[i-m]-'a']--;
+            if(mp1==mp2) return true;
         }
         return false;
     }
