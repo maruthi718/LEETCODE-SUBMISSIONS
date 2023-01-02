8b4d24194f75bb1e08aa01937ec28d059ddfa5e3
Maruthi Kunuku <113303489+maruthi718@users.noreply.github.com>
2023-01-02 21:54:28 +0530

    Time: 5 ms (78.86%), Space: 9.7 MB (26.49%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -9,18 +9,8 @@ public:
             return n;
         while(low<=high)
         {
-            if(low+1==high) 
-            {
-                if(nums[low]==target) return low;
-                else if(nums[high]==target) return high;
-                else if(target<nums[low]) return low-1;
-                else if(target>nums[high]) return high+1;
-                 else if(target>nums[low] && target<nums[high]) return low+1;
-            }
             int mid=(low+high)/2;
             if(nums[mid]==target) return mid;
-            else if(target > nums[mid-1] && target<nums[mid]) return mid;
-            else if(target > nums[mid] && target<nums[mid+1]) return mid+1;
             else if(target<nums[mid])
             {
                 high=mid-1;
@@ -28,6 +18,6 @@ public:
             else
                 low=mid+1;
         }
-        return 0;
+        return low;
     }
 };
