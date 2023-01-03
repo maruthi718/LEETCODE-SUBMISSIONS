e838c8958bb21d68b506871c8a29d4e2cc853499
Maruthi Kunuku <113303489+maruthi718@users.noreply.github.com>
2023-01-03 23:03:04 +0530

    Time: 28 ms (84.91%), Space: 22.3 MB (58.93%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -7,11 +7,25 @@ public:
         while(low<=high)
         {
             mid=(low+high)/2;
-            if(low==high==n-1) return nums[n-1];
-            if(nums[mid]==nums[mid^1])
-                low=mid+1;
-            else
-                high=mid-1;
+            if(low==high==n-1) return nums[low];
+            if(mid%2==0)
+            {
+                if(nums[mid+1]==nums[mid])
+                {
+                    low=mid+1;
+                }
+                else
+                  high=mid-1;
+            }
+            else 
+            {
+                if(nums[mid-1]==nums[mid])
+                {
+                    low=mid+1; 
+                }
+                else
+                   high=mid-1; 
+            }
         }
         return nums[low];
     }
