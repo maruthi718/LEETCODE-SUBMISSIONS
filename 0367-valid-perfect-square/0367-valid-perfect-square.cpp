fd046e61046234c7dbeeff5fc52efd201212021e
Maruthi Kunuku <113303489+maruthi718@users.noreply.github.com>
2023-02-02 23:38:06 +0530

    Time: 3 ms (46.24%), Space: 6 MB (48.96%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -3,9 +3,10 @@ public:
     bool isPerfectSquare(int n) {
         if(n==1) return true;
          int low=1,high=n/2;
+        long long int mid;
         while(low<=high)
         {
-            long long int mid=low+(high-low)/2;
+             mid=(low+high)>>1;
             long long int k=mid*mid;
             if(k==n)
                 return true;
