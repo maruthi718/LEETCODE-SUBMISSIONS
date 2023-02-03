5bf19ea5dee7caadb79cb6bfb44cb3eda484e18b
Maruthi Kunuku <113303489+maruthi718@users.noreply.github.com>
2023-02-03 17:03:58 +0530

    Time: 38 ms (47.61%), Space: 9.1 MB (14.00%) - LeetHub

---
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarrayGCD(vector<int>& nums, int k) {
+        int cnt=0;
+        int n=nums.size(),x,curr;
+        for(int i=0;i<n;i++)
+        {
+            curr=nums[i];
+            for(int j=i;j<n;j++)
+            {
+                if(curr%k!=0 && curr>k) break;
+                curr=__gcd(curr,nums[j]);
+                if(curr==k) cnt++;
+            }
+        }
+        return cnt;
+    }
+};
