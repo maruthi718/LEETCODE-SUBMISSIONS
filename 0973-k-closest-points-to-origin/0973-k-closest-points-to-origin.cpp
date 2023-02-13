3462ee2349a385fd102a65c182715c715fff97da
Maruthi Kunuku <113303489+maruthi718@users.noreply.github.com>
2023-02-14 00:13:18 +0530

    Time: 420 ms (32.58%), Space: 75.1 MB (33.10%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    double distance(int x,int y)
+    int distance(int x,int y)
     {
         int res;
         res=(x*x)+(y*y);
-        return sqrt(res);
+        return res;
     }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>>ans;
-        map<double,vector<vector<int>>>mp;
+        priority_queue<vector<int>>pq;
         for(int i=0;i<points.size();i++)
         {
-            double d=distance(points[i][0],points[i][1]);
-            mp[d].push_back(points[i]);
+            int d=distance(points[i][0],points[i][1]);
+            pq.push({d,points[i][0],points[i][1]});
+                     if(pq.size()>k)
+                         pq.pop();
         }
-        for(auto it:mp)
+        while(!pq.empty())
         {
-            for(auto i:it.second)
-            {
-                ans.emplace_back(i);
-                 if(ans.size()==k) break;
-            }
-            if(ans.size()==k) break;
+            vector<int>temp=pq.top();
+            ans.push_back({temp[1],temp[2]});
+            pq.pop();
         }
       return ans;
     }
